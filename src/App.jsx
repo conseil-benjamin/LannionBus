22904ca9007bf409df7a5504155879a27a88965b
Benjamin <benlenormand61@gmail.com>
2024-10-02 18:29:49 +0200

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ function App() {
   const seconds = timer % 60;
 
   useEffect(() => {
+    //test
     const today = new Date();
     const day = String(today.getDate()).padStart(2, '0');
     const month = String(today.getMonth() + 1).padStart(2, '0');
